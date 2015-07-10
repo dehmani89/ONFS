1adb08ea6824e8cdec988f18a1d49a3f0e5aa3d1
dehmani89 <amine.dehmani@gmail.com>
2015-07-10 10:29:08 -0400

    added new file say hi

---
+++ MVC_JSP/src/com/amine/SayHi.java
@@ -0,0 +1,23 @@
+package com.amine;
+
+public class SayHi {
+	
+	String message1 = "Hello from Message 1";
+
+
+	public SayHi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	public String getMessage1() {
+		return message1;
+	}
+
+	public void setMessage1(String message1) {
+		this.message1 = message1;
+	}
+	
+}
